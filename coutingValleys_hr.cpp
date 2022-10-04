2d2cf2aee828358ea79ae953793e87271e346021
amankayat <72142181+amankayat@users.noreply.github.com>
2022-10-04 13:53:39 +0530

    adding hackerrank prob

---
+++ coutingValleys_hr.cpp
@@ -0,0 +1,37 @@
+platform:
+Hackerrank
+
+problem link:
+https://www.hackerrank.com/challenges/counting-valleys/problem
+
+code:
+#include<iostream>
+#include<stdio.h>
+#include <cstdlib>
+using namespace std;
+int counting_valleys(int n,string str){
+	int count = 0;
+	int valley=0;
+	for(int i=0;i<n;i++){
+		if(str[i]=='u'||str[i]=='U'){
+			count+=1;
+			if(count==0)
+			 valley++;
+		
+		}else {
+			count-=1;
+ 		
+		}
+		
+	}
+	return abs(valley);
+}
+int main(){
+	int n;
+	string str;
+	cin>>n;
+	cin>>str;
+
+	int result = counting_valleys(n,str);
+	cout<<result;
+}
